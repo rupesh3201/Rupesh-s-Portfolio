77a7f26249d7cc4a4deac8413b4d3d12e9081f33
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-10-24 22:22:32 +0530

    postcss config

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,4 +1,4 @@
-import  { useState } from "react"; // Import useState
+
 import PreHero from "./Pages/PreHero"; // Ensure you import PreHero
 import Hero from "./Pages/Hero";
 import AboutPage from "./Pages/AboutPage";
